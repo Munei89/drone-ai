f97eccb13cca8258f5bc7735c55700d4dfa8c927
Munei Nengwenani <>
2019-03-17 15:35:16 +0200

    More dependancies  :-)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDom from "react-dom"
+import "@babel/polyfill"
+import App from "./App"
+import "./index.css"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (process.env.NODE_ENV === "development") {
+  const axe = require("react-axe")
+  axe(React, ReactDom, 1000)
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDom.render(
+      <App />,
+  document.getElementById("app")
+)
