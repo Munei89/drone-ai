10cb3c16328454cb2ed6d47f92f59f77f98cee2c
Munei Nengwenani <>
2019-03-17 15:00:51 +0200

    Intial commit with dev dependancies

---
+++ webpack.config.base.js
@@ -0,0 +1,48 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "main.bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader", "postcss-loader"]
+      },
+      {
+        test: /\.svg$/,
+        loader: ["svg-inline-loader"]
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              mimetype: "image/png"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+}
---
+++ webpack.config.prod.js
@@ -0,0 +1,15 @@
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.config.base')
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
+
+module.exports = merge(baseConfig, {
+    mode: 'production',
+    plugins: [new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        openAnalyzer: false
+    })],
+    externals: {
+        react: 'React',
+        'react-dom': 'ReactDOM'
+    }
+})
